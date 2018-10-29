066a4a228916465b02e509e81634d52015df757d
sanboxrunner <jjspira@gmail.com>
2018-10-29 13:05:36 -0400

    Added camera. Using cinemachine. Good idea? We'll see!

--- Assets/_Scripts/DeadzoneCamera.cs
+++ Assets/_Scripts/DeadzoneCamera.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
+// using UnityEditor;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 [RequireComponent(typeof(Camera))]
 public class DeadzoneCamera : MonoBehaviour {
@@ -68,33 +66,31 @@ public class DeadzoneCamera : MonoBehaviour {
     }
 }
 
-#if UNITY_EDITOR
+// [CustomEditor(typeof(DeadzoneCamera))]
+// [CanEditMultipleObjects]
+// public class DeadZoneEditor : Editor {
+//     public void OnSceneGUI() {
+//         DeadzoneCamera cam = target as DeadzoneCamera;
 
-[CustomEditor(typeof(DeadzoneCamera))]
-public class DeadZonEditor : Editor {
-    public void OnSceneGUI() {
-        DeadzoneCamera cam = target as DeadzoneCamera;
+//         Vector3[] vert = {
+//             cam.transform.position + new Vector3(cam.deadzone.xMin, cam.deadzone.yMin, 0),
+//             cam.transform.position + new Vector3(cam.deadzone.xMax, cam.deadzone.yMin, 0),
+//             cam.transform.position + new Vector3(cam.deadzone.xMax, cam.deadzone.yMax, 0),
+//             cam.transform.position + new Vector3(cam.deadzone.xMin, cam.deadzone.yMax, 0)
+//         };
 
-        Vector3[] vert = {
-            cam.transform.position + new Vector3(cam.deadzone.xMin, cam.deadzone.yMin, 0),
-            cam.transform.position + new Vector3(cam.deadzone.xMax, cam.deadzone.yMin, 0),
-            cam.transform.position + new Vector3(cam.deadzone.xMax, cam.deadzone.yMax, 0),
-            cam.transform.position + new Vector3(cam.deadzone.xMin, cam.deadzone.yMax, 0)
-        };
+//         Color transp = new Color(0, 0, 0, 0);
+//         Handles.DrawSolidRectangleWithOutline(vert, transp, Color.red);
 
-        Color transp = new Color(0, 0, 0, 0);
-        Handles.DrawSolidRectangleWithOutline(vert, transp, Color.red);
+//         for (int i = 0; i < cam.limits.Length; ++i) {
+//             Vector3[] vertLimit = {
+//                 new Vector3(cam.limits[i].xMin, cam.limits[i].yMin, 0),
+//                 new Vector3(cam.limits[i].xMax, cam.limits[i].yMin, 0),
+//                 new Vector3(cam.limits[i].xMax, cam.limits[i].yMax, 0),
+//                 new Vector3(cam.limits[i].xMin, cam.limits[i].yMax, 0)
+//             };
 
-        for (int i = 0; i < cam.limits.Length; ++i) {
-            Vector3[] vertLimit = {
-                new Vector3(cam.limits[i].xMin, cam.limits[i].yMin, 0),
-                new Vector3(cam.limits[i].xMax, cam.limits[i].yMin, 0),
-                new Vector3(cam.limits[i].xMax, cam.limits[i].yMax, 0),
-                new Vector3(cam.limits[i].xMin, cam.limits[i].yMax, 0)
-            };
-
-            Handles.DrawSolidRectangleWithOutline(vertLimit, transp, Color.green);
-        }
-    }
-}
-#endif
+//             Handles.DrawSolidRectangleWithOutline(vertLimit, transp, Color.green);
+//         }
+//     }
+// }
